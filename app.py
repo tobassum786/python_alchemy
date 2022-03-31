a16c9aa2e889a17c389ae17f48084508c54b4395
Tobassum Munir <tobassum@msn.com>
2022-03-31 22:41:53 +0300

    making tables and make relations

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+
+from datetime import datetime
+
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
@@ -7,6 +12,32 @@ app.config['SECRET_KEY'] = '3UPijOcTgh7oo7qBR0cDsA'
 
 db = SQLAlchemy(app)
 # db.init_app(app)
+
+# Making Tables
+class Customer(db.Model):
+	id = db.Column(db.integer, primary_key=True)
+	first_name = db.Column(db.string(50), nullable=False)
+	last_name = db.Column(db.string(50), nullable=False)
+	address = db.Column(db.string(500), nullable=False)
+	city = db.Column(db.string(500), nullable=False)
+	postcode = db.Column(db.string(50), nullable=False)
+	email = db.Column(db.string(50), nullable=True, unique=True)
+
+
+class Order(db.Model):
+	id = db.Column(db.integer, primary_key=True)
+	order_date = db.Column(db.DateTime, nullable=False defualt=datetime.utcnow())
+	shipped_date = db.Column(db.DateTime, datetime)
+	delivered_date = db.Column(db.DateTime, datetime)
+	cupon_code = db.Column(db.string(50))
+
+
+
+class Product(db.Model):
+	id = db.Column(db.integer, primary_key=True)
+	name = db.Column(db.string(50), nullable=False, unique=True)
+	price = db.Column(db.integer, nullable=False)
+
 
 
 @app.route('/')
